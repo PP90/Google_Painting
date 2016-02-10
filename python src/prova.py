24c221a5f2fd1876368e7d461bf132bce4d25517
pietro <pietropiscione@gmail.com>
2016-02-10 22:55:08 +0100

    The raw content has a list of string. Each string is a row from the file.
    TO DO:
    to convert the list in a char matrix.

--- python src/prova.py
+++ python src/prova.py
@@ -19,15 +19,24 @@ def import_file_painting(filename):##The file content is raw format. It is neede
 	content=list(open(filename))
 	return content
 
-def get_size_img(raw_content):
-	first_row=raw_content[0]
+def get_size_img(first_row):
 	pos=first_row.find(' ')
 	cols_number=first_row[pos+1:len(first_row)-1]
 	rows_number=first_row[0:pos]
 	return int(rows_number), int(cols_number)
 
+def remove_return_characters(raw_content, n_cols):
+	for idx, element in enumerate(raw_content):
+		raw_content[idx]=element[0:n_cols-1]
+	return raw_content
+	
+def list_to_matrix_char(raw_content, n_rows, n_cols):
+		print "Roba"
+
 def test():
 	raw_content=import_file_painting("logo.in")	
-	get_size_img(raw_content)
+	n_rows, n_cols= get_size_img(raw_content[0])
+	raw_content.pop(0)
+	raw_content=remove_return_characters(raw_content, n_cols)
 
 test()
