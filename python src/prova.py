2cae575f0f8b6b4ba39a3e20fa4016f5a8709f85
pietro <pietropiscione@gmail.com>
2016-02-10 17:14:57 +0100

    Build the source skeleton

---
+++ python src/prova.py
@@ -0,0 +1,16 @@
+###Python soruce code for google painting problem
+
+def paint_square(rows, columns, s):
+	if(rows<1 or columns <1 or  s<1):
+		print "Error"
+		return -1
+
+def paint_line(r1, c1, r2, c2):
+	if(r1<1 or c1 <1):
+		print "Error"
+		return -1
+
+def erease_cell(row, column):
+	if(row<0 or column<0)
+		print "Error"
+		return -1 
