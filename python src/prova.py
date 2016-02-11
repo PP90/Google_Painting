f11093e5e79c1d0e1d1152f3a3b2345a5272f62e
pietro <pietropiscione@gmail.com>
2016-02-11 18:23:55 +0100

    The size of the subimage is now available.

--- python src/prova.py
+++ python src/prova.py
@@ -1,9 +1,10 @@
 ###Python soruce code for google painting problem
 
-HORIZONTAL_WAY=60
-VERTICAL_WAY=22
-UP=12
-DOWN=34
+HORIZ_LEFT=60
+HORIZ_RIGHT=22
+
+VER_UP=53
+VER_DOWN=34
 
 def paint_square(rows, columns, s):
 	if(rows<1 or columns <1 or  s<1):
@@ -66,10 +67,15 @@ def get_sub_image(matrix_char, start_row, end_row, start_column, end_column):
 	return sub_image
 
 def get_size_sub_image(sub_image):
-	print "to do"
+	n_rows=len(sub_image)
+	n_cols=0
+	for element in sub_image:
+		n_cols=len(element)
+		break
+	return n_rows, n_cols
 
 def recognize_row(sub_image, mode):
-		print "to do"
+		print "cacca"
 
 def char_matrix2string_list(char_matrix):
 	string_list=[]
@@ -83,10 +89,17 @@ def test():
 	raw_content.pop(0)##The first row is removed
 	raw_content=remove_return_characters(raw_content, n_cols)##the return character are removed
 	char_matrix=list_to_matrix_char(raw_content, n_rows, n_cols)##The raw content-char matrix conversion is performed	
-	sub_image=get_sub_image(char_matrix, 10,20,10,20)##giving the coordinates a submatrix is extracted from the char matrix
-	string_list=char_matrix2string_list(sub_image)
-	print_pretty(string_list)
-	#print_pretty(sub_image)
+	sub_image=get_sub_image(char_matrix, 0,20,0,20)##giving the coordinates a submatrix is extracted from the char matrix
+	print get_size_sub_image(sub_image)
 
+	
+	'''
+	DEBUG PRINTS
+	string_list=char_matrix2string_list(sub_image)
+	string_list2=char_matrix2string_list(char_matrix)
+	print_pretty(string_list)
+	print_pretty(string_list2)
+	#print_pretty(sub_image)
+	'''
 	
 test()
